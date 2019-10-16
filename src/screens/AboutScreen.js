169c4d7cbf0d4f117609b0fa677ebced5566919b
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-10-16 18:18:08 -0500

    about
    about screen info

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default AboutScreen = () => {
     return (
       <View style={styles.about}>
-        <Text>About Screen</Text>
+        <Text style={styles.title}>Sobre DatApiApp:</Text>
+        <Text style={styles.text}>{
+          `Esta app la realice como practica de solución en desarrollo móvil. Trata sobre el acceso y consulta a datos abiertos del gobierno mexicano usando la api de la plataforma de registros de información pública en organizaciones federales, estatales, municipales y autónomas.\n\n\n\n- Esta app y su código queda de acceso libre para quien desee usarlo.
+          `}
+        </Text>
       </View>
     );
 }
@@ -12,6 +16,17 @@ const styles = StyleSheet.create({
   about: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  title: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  text: {
+    textAlign: 'justify',
+    padding: 8,
+    color: 'black',
+    fontSize: 16
+  }  
 });
